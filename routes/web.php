8c64fdd4f5e9b112cae1179f202340fbc629350d
hitesh <hiteshr@zignuts.com>
2023-05-01 20:14:58 +0530

    added register, login, forgot password, reset password, verify email, logout group routes added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->prefix('auth')->group(function () {
+    Route::get('register-form', 'registerForm')->name('register.view');
+    Route::post('register', 'register')->name('register');
+    Route::get('login-form', 'loginForm')->name('login.view');
+    Route::post('login', 'login')->name('login');
+    Route::get('dashboard', 'dashboard')->name('dashboard');
+    Route::get('verify-email/{token}', 'verifyEmail')->name('verify.email');
+    Route::get('forgot-password-form', 'forgotPasswordForm')->name('forgot.password.view');
+    Route::post('forgot-password', 'forgotPassword')->name('forgot.password');
+    Route::get('reset-password/{token}', 'resetPasswordForm')->name('reset.password.view');
+    Route::post('reset-password', 'resetPassword')->name('reset.password');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::controller(UserController::class)->prefix('user')->group(function () {
+        Route::get('logout', 'logout')->name('logout');
+    });
 });
