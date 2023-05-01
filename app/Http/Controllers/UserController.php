1231b09077584e91f7f9a0fe10f3844ae4ab5312
hitesh <hiteshr@zignuts.com>
2023-05-01 20:07:36 +0530

    UserController added and logout method for user logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function logout()
+    {
+        session()->flush();
+        Auth::logout();
+        return redirect()->route('login.view')->withSuccess('You logged out successfully');
+    }
 }
